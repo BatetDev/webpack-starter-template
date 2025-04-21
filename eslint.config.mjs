0dda154a79bcdc1e6713009a4310985c7f894652
fr4nbtt <batet.francisco@gmail.com>
2025-04-21 19:50:41 -0300

    chore: Format codebase with Prettier to enforce single quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import prettierPlugin from "eslint-plugin-prettier";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js, prettier: prettierPlugin },
-    extends: ["js/recommended", "plugin:prettier/recommended"],
+    extends: ['js/recommended', 'plugin:prettier/recommended'],
     rules: {
-      "prettier/prettier": "warn",
+      'prettier/prettier': 'warn',
     },
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
 ]);
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'development',
+  devtool: 'eval-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
     hot: true,
-    watchFiles: ["./src/template.html"],
+    watchFiles: ['./src/template.html'],
   },
 });
