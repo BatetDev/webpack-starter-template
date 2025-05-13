199e94c0c1b3d0f75de2ac1117e81d2b085c06df
fr4nbtt <batet.francisco@gmail.com>
2025-05-13 20:01:59 -0300

    feat: integrate tailwindcss v4 and remove normalize.css

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../styles/main.css';
+import '../styles/output.css';
 import { createIcons, icons } from 'lucide';
 
 import { greet } from './modules/utils.js';
@@ -12,7 +12,8 @@ greetBabel('World');
 
 const iconSpan = document.getElementById('icon');
 if (iconSpan) {
-  iconSpan.innerHTML = '<i data-lucide="landmark" width="40" height="40"></i>';
+  iconSpan.innerHTML =
+    '<i data-lucide="landmark" width="40" height="40" class="inline"></i>';
   createIcons({ icons });
 } else {
   console.warn('No element with id="icon" found.');
